a9cb250ddcfe96eb59578206334d6aec71947849
Breno-AA <breno_de_avila@outlook.com>
2023-12-07 16:42:53 -0300

    add addPost func

--- route.go
+++ route.go
@@ -30,3 +30,20 @@ func getPosts(resp http.ResponseWriter, req *http.Request) {
 	resp.WriteHeader(http.StatusOK)
 	resp.Write(result)
 }
+
+func addPost(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-type", "application/json")
+	var post Post
+	err := json.NewDecoder(req.Body).Decode(&post)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"error: Error unmarshalling the request}`))
+		return
+	}
+
+	post.Id = len(posts) + 1
+	posts = append(posts, post)
+	resp.WriteHeader(http.StatusOK)
+	result, err := json.Marshal(post)
+	resp.Write(result)
+}
