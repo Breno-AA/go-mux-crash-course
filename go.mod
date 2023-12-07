dc717583a1b98e77d47df360b76764b91a2d62d9
Breno-AA <breno_de_avila@outlook.com>
2023-12-07 16:09:29 -0300

    add server file

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-mux-crash-course
+
+go 1.21.5
+
+require github.com/gorilla/mux v1.8.1
---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	const port string = ":8080"
+	router.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+		fmt.Fprintln(resp, "Server Running...")
+	})
+	log.Println("Server listening on port", port)
+	log.Fatalln(http.ListenAndServe(port, router))
+
+}
