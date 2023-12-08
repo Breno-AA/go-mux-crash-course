7b48ad43859d79a76e83369f1da2b168da584ee7
Breno-AA <breno_de_avila@outlook.com>
2023-12-08 11:48:55 -0300

    add controller layer

---
+++ controller/post-controller.go
@@ -0,0 +1,67 @@
+package controller
+
+import (
+	"encoding/json"
+	"go-mux-crash-course/entity"
+	"go-mux-crash-course/errors"
+	"go-mux-crash-course/service"
+	"net/http"
+)
+
+type controller struct{}
+
+var (
+	postService service.PostService
+)
+
+type PostController interface {
+	GetPosts(response http.ResponseWriter, request *http.Request)
+	AddPost(response http.ResponseWriter, request *http.Request)
+}
+
+func NewPostController(service service.PostService) PostController {
+	postService = service
+	return &controller{}
+}
+
+func (*controller) GetPosts(response http.ResponseWriter, req *http.Request) {
+	response.Header().Set("Content-type", "application/json")
+	posts, err := postService.FindAll()
+
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(response).Encode(errors.ServiceError{Message: "Error getting the posts"})
+
+	}
+	response.WriteHeader(http.StatusOK)
+	json.NewEncoder(response).Encode(posts)
+}
+
+func (*controller) AddPost(response http.ResponseWriter, req *http.Request) {
+	response.Header().Set("Content-type", "application/json")
+	var post entity.Post
+	err := json.NewDecoder(req.Body).Decode(&post)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(response).Encode(errors.ServiceError{Message: "Error unmarshalling data"})
+		return
+	}
+
+	err1 := postService.Validate(&post)
+	if err1 == nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(response).Encode(errors.ServiceError{Message: err1.Error()})
+
+	}
+
+	result, err2 := postService.Create(&post)
+	if err2 != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(response).Encode(errors.ServiceError{Message: "Error saving the post"})
+
+	}
+
+	response.WriteHeader(http.StatusOK)
+	json.NewEncoder(response).Encode(result)
+
+}
